dc6a63bfcea752f691a0505b2063efe1e56c8ae4
Dương Hồng Đức <yangdungde123@gmail.com>
2019-03-08 11:09:33 +0700

    a

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sshpass -p 'vagrant' ssh -tt root@192.168.100.19 <<EOF
+sshpass -p 'vagrant' ssh -tt -o StrictHostKeyChecking=no root@192.168.100.19 <<EOF
   cd /home/ducdh/app/CICD-Django/cicd
   source ../env/bin/activate
   git pull
