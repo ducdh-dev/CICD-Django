51d56170c14ae6348189a73c34c17bfe6d89fa25
Dương Hồng Đức <yangdungde123@gmail.com>
2019-03-08 14:21:35 +0700

    ??

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-whoami
-cd /home/ducdh/app/CICD-Django/cicd
-source ../env/bin/activate
-git pull
-pip install -r requirements.txt
-python manage.py migrate
-exit
+
+sshpass -p 'vagrant' ssh -tt -o StrictHostKeyChecking=no root@192.168.100.19 <<EOF
+  cd /home/ducdh/app/CICD-Django/cicd
+  source ../env/bin/activate
+  git pull
+  pip install -r requirements.txt
+  python manage.py migrate
+  exit
+EOF
